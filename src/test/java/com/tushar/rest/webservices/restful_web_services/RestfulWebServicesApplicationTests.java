73b9b3f428cfab0826d8c40ed9d1d20cbce0e7d6
Tushar-Gahlaut_indigo <tushargahlaut74@gmail.com>
2024-07-09 16:58:22 +0530

    setup for sample project

---
+++ src/test/java/com/tushar/rest/webservices/restful_web_services/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tushar.rest.webservices.restful_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
